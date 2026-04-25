74e6f32f4271c7e9cb5bbb5120f500e825b4f809
Renato D. Luciano <renatoluciano@gmail.com>
2026-04-25 15:19:16 -0300

    feat: Adicionados modelos de Categoria e Produto

--- restaurantech/admin.py
+++ restaurantech/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Mesa
+from .models import Mesa, Categoria, Produto
 
 admin.site.register(Mesa)
+admin.site.register(Categoria)
+admin.site.register(Produto)
--- restaurantech/models.py
+++ restaurantech/models.py
@@ -18,3 +18,20 @@ class Mesa(models.Model):
 
     def __str__(self):
         return f"Mesa {self.numero}"
+    
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    disponivel = models.BooleanField(default=True) # Para pausar vendas se acabar o estoque
+
+    def __str__(self):
+        return f"{self.nome} - R$ {self.preco}"
+

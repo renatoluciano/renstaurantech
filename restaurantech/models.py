83630ef5c345f2ed4890cc2956a1b55c1dcd0e73
Renato D. Luciano <renatoluciano@gmail.com>
2026-04-25 15:11:24 -0300

    feat: Adicionado modelo de Mesa e migrações iniciais

--- restaurantech/models.py
+++ restaurantech/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Mesa(models.Model):
+    # Opções de status para a mesa
+    STATUS_CHOICES = [
+        ('LIVRE', 'Livre'),
+        ('OCUPADA', 'Ocupada'),
+        ('CONTA', 'Aguardando Conta'),
+    ]
+
+    numero = models.IntegerField(unique=True)
+    capacidade = models.IntegerField()
+    status = models.CharField(
+        max_length=10, 
+        choices=STATUS_CHOICES, 
+        default='LIVRE'
+    )
+
+    def __str__(self):
+        return f"Mesa {self.numero}"
